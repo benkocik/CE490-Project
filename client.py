426cbfa1732a6856a6a2555a9e0ec31a1f4a7ed7
Ben Kocik <bkocik@heestand.tech>
2021-04-28 15:45:09 -0400

    Started client and server programs

---
+++ client.py
@@ -0,0 +1,14 @@
+'''
+Programmers: Ben Kocik, Parker Authier, Ava Zaremski
+Project Description: Code for a device that provides illuminated directional information for building hallways
+Code Description: Client code to receive information from server and other clients/nodes.
+'''
+
+# Imports
+
+# Main function definition
+def main():
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,14 @@
+'''
+Programmers: Ben Kocik, Parker Authier, Ava Zaremski
+Project Description: Code for a device that provides illuminated directional information for building hallways
+Code Description: Server to communicate to all clients or nodes.  Will also receive information from nodes.
+'''
+
+# Imports
+
+# Main function definition
+def main():
+
+
+if __name__ == "__main__":
+    main()
