610a05bf45fcb5d5695b86a2d677684f755464c2
Ben Kocik <bkocik@heestand.tech>
2021-05-10 17:46:11 -0400

    added wheel for rainbow

--- LED_Control/LED_Control.py
+++ LED_Control/LED_Control.py
@@ -48,6 +48,27 @@ def pulse(length, num, r, g, b, direction="left"):
 #                led = num
 #            for led 
 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
 def rainbow_cycle(num):
     for j in range(255):
         for i in range(num):
