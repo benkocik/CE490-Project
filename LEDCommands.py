1788bedca016c53be1dc04f9dafba2913e4b3554
Ben Kocik <bkocik@heestand.tech>
2021-06-10 02:27:46 -0400

    fix

--- LEDCommands.py
+++ LEDCommands.py
@@ -91,17 +91,17 @@ def arrived(pixels, color, length = 3, wait_ms = 50):
         # Beginning side
         for n in range(length):
             if i-n >= 0 and i+n < len(pixels):
-                pixels[i+n] = color
+                pixels[i-n] = color
             # Turn off pixels behind
-            if (i-(length)) >= 0:
-                pixels[i-(length)] = (0, 0, 0)
+            if ((i-length)-1) >= 0:
+                pixels[(i-length)-1] = (0, 0, 0)
         # End side
         for k in range(length, -1, -1):
             if i-n >= 0 and i+n < len(pixels):
-                pixels[(len(pixels)-1) - (i+k)] = color
+                pixels[((len(pixels)-1)-i)+k] = color
             # Turn off pixels behind
-            if (i+length) < len(pixels):
-                pixels[i+length+1] = (0, 0, 0)
+            if (((len(pixels)-1)-i)+length+1) < len(pixels):
+                pixels[((len(pixels)-1)-i)+length+1] = (0, 0, 0)
         time.sleep(wait_ms / 1000.0)
     turnOff(pixels)
 
